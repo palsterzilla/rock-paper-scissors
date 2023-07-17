ee5a988bf0ead00ac24ac520502b602c17232a2d
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-17 21:40:58 +0700

    add playRound function

--- game.js
+++ game.js
@@ -2,6 +2,42 @@ function getComputerChoice() {
   const gameChoice = ["rock", "paper", "scissors"];
   let randomChoice = gameChoice[Math.floor(Math.random() * gameChoice.length)];
   
-  // console.log(gameChoice[randomChoice]);/
+  console.log(randomChoice);
   return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  const playerWin = `You Win! ${playerSelection} beats ${computerSelection}`;
+  const playerLose = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  
+  if (playerSelection == "rock") {
+    if (computerSelection == "paper") {
+      return playerLose;
+    } else if (computerSelection == "scissors") {
+      return playerWin;
+    } else {
+      return "Draw";
+    }
+  } else if (playerSelection == "scissors") {
+      if (computerSelection == "rock") {
+        return playerLose;
+      } else if (computerSelection == "paper") {
+          return playerWin;
+      } else {
+        return "Draw";
+      }
+  } else if (playerSelection == "paper") {
+    if (computerSelection == "scissors") {
+      return playerLose;
+    } else if (computerSelection == "rock") {
+        return playerWin;
+    } else {
+      return "Draw";
+    }
+  }
+
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
