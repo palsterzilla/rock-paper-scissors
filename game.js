9957672212b8b6e0df78b65ff1804d9a2691454b
Firyal Naufal <naufal.firyal@gmail.com>
2023-07-18 12:11:59 +0700

    Implement winner counter

--- game.js
+++ game.js
@@ -2,7 +2,9 @@ function getComputerChoice() {
   const gameChoice = ["rock", "paper", "scissors"];
   let randomChoice = gameChoice[Math.floor(Math.random() * gameChoice.length)];
   
+  // just a helper, delete later
   console.log(`computer pick: ${randomChoice}`);
+  
   return randomChoice;
 }
 
@@ -40,21 +42,25 @@ function playRound(playerSelection, computerSelection) {
  
 function game() {
   let playerWinCount = 0;
+  let computerWinCount = 0;
   
   for (let i = 0; i < 5; i++) {
     const playerSelection = prompt("Pick one of the following; rock, paper or scissors").toLowerCase();
     const computerSelection = getComputerChoice();
+    // const computerSelection = "rock";
 
     console.log(playRound(playerSelection, computerSelection));
-  
-    // TODO: implement player win count
-    // if (playRound(playerSelection, computerSelection) == /You Win!*/) {
-    //   playerWinCount++;
-    //   console.log("menang coy")
-    // }
+    
+    if (/You Win/.test(`${playRound(playerSelection, computerSelection)}`)) {
+      playerWinCount++;
+    } else if (/You Lose/.test(`${playRound(playerSelection, computerSelection)}`)) {
+      computerWinCount++;
+    }
   }
-
-  // console.log(playerWinCount);
+  
+  // TODO: implement when win lose or draw
+  let result = playerWinCount > computerWinCount ? "Player Win!" : "Computer Win!";
+  console.log(result);
 }
 
 game();
