705c23396bbc03edecd85a4db6633048ebdfb439
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-03 14:24:49 +0700

    Feature: Replace prompt with 3 button options

--- game.js
+++ game.js
@@ -7,46 +7,45 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   const playerWin = `You Win! ${playerSelection} beats ${computerSelection}`;
-  const playerLose = `You Lose! ${computerSelection} beats ${playerSelection}`;  
+  const playerLose = `You Lose! ${computerSelection} beats ${playerSelection}`;
 
   if (playerSelection == computerSelection) {
-    return "Draw"
+    console.log("Draw")
 
   } else if (
       (playerSelection == "rock" && computerSelection == "paper") ||
       (playerSelection == "paper" && computerSelection == "scissors") ||
       (playerSelection == "scissors" && computerSelection == "rock")) {
 
-        return playerLose
+        console.log(playerLose)
 
   } else if (
       (computerSelection == "rock" && playerSelection == "paper") ||
       (computerSelection == "paper" && playerSelection == "scissors") ||
       (computerSelection == "scissors" && playerSelection == "rock")) {
         
-        return playerWin
+        console.log(playerWin)
 
   } else {
-    return "Incorrect typing!"
+    console.log("Incorrect typing!")
   }
 
 }
  
+// might not be used anymore
 function game() {
   let playerWinCount = 0;
   let computerWinCount = 0;
-  
-  for (let i = 1; i <= 5; i++) {
-    const playerSelection = prompt("Pick one of the following; rock, paper or scissors").toLowerCase();
-    const computerSelection = getComputerChoice();
 
-    console.log(`Round ${i}: ${playRound(playerSelection, computerSelection)}`);
-    
-    if (/You Win/.test(`${playRound(playerSelection, computerSelection)}`)) {
-      playerWinCount++;
-    } else if (/You Lose/.test(`${playRound(playerSelection, computerSelection)}`)) {
-      computerWinCount++;
-    }
+  const playerSelection = prompt("Pick one of the following; rock, paper or scissors").toLowerCase();
+  const computerSelection = getComputerChoice();
+
+  console.log(`Round ${1}: ${playRound(playerSelection, computerSelection)}`);
+  
+  if (/You Win/.test(`${playRound(playerSelection, computerSelection)}`)) {
+    playerWinCount++;
+  } else if (/You Lose/.test(`${playRound(playerSelection, computerSelection)}`)) {
+    computerWinCount++;
   }
   
   if (playerWinCount > computerWinCount ) {
@@ -56,8 +55,23 @@ function game() {
   } else {
     return console.log("Draw!")
   }
-
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => button.addEventListener("click", game))
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+
+buttonRock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice())
+})
+
+buttonPaper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice())
+})
+
+buttonScissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice())
+})
+
+// const buttons = document.querySelectorAll("button");
+// buttons.forEach(button => button.addEventListener("click", game))
